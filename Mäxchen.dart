6925aff50dc81b2b1f312b45d31cc71d6d85283a
VolkanAppAkademie <supafly_69@gmx.de>
2024-12-03 23:04:10 +0100

    commented

--- Mäxchen.dart
+++ Mäxchen.dart
@@ -1,17 +1,23 @@
 void main() {
-  int a = 1;
-  int b = 2;
+  int a = 2;
+  int b = 4;
 
+  // Tausche Werte, wenn a kleiner als b ist
   if (a < b) {
     int temporary = a;
     a = b;
     b = temporary;
   }
 
+  // Prüfe auf "Pasch"
   if (a == b) {
     print('Pasch $a');
+
+    // Spezielle Ausgabe für Mäxchen
   } else if (a == 2 && b == 1) {
     print('Mäxchen');
+
+    // Standardfall: Ausgabe der beiden Zahlen
   } else {
     print('$a$b');
   }
